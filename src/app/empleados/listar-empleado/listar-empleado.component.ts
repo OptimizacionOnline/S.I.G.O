8ac6e5c83d65af24c72448a7667696274d48b659
Diana Laura <diana.laura.ps.99@gmail.com>
2022-03-29 13:14:01 -0600

    Edicion clientes paginacion

--- src/app/empleados/listar-empleado/listar-empleado.component.ts
+++ src/app/empleados/listar-empleado/listar-empleado.component.ts
@@ -1,249 +1,249 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Empleados {
-  Num?: number;
-  Nombre: string;
-  Email: string;
-  Departamento: string;
-  Oficina: string;
-  Rol:string;
+    Num?: number;
+    Nombre: string;
+    Email: string;
+    Departamento: string;
+    Oficina: string;
+    Rol:string;
 }
 const EMPLEADO: Empleados[] = [
-  {
-      Nombre: "Maria Fernanda Mercado Garcia",
-      Email: "fernanda.mercado@optimizacion-online.com",
-      Departamento: "Asistente de direccion general",
-      Oficina: "Leon",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Josselyn Sarahi Nepomuceno",
-      Email: "josselyn.nepomuceno@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Guadalupe Chacon Acosta",
-      Email: "guadalupe.chacon@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Viviana Velasco Montero",
-      Email: "viviana.velasco@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Emmanuel Garcia Solis",
-      Email: "emmanuel.garcia@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Isai Lopez Fuentes",
-      Email: "isai.lopez@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Raul Rios Conde",
-      Email: "raul@optimizacion-online.com",
-      Departamento: "CEO",
-      Oficina: "Leon",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Natalia Daniela Sanchez Garcia",
-      Email: "daniela.garcia@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Colombia",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Flor Noemi Fernandez Ceron",
-      Email: "noemi.fernandez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Ciudad de Mexico",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Irian Valeria Perez Romero",
-      Email: "irian.perez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Ciudad de Mexico",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Victorio Maniel Molina",
-      Email: "victorio.molina@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Argentina",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Emmy Julissa Enriquez Salazar",
-      Email: "emmy.enriquez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Merida",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Vanessa Alexandra Garcia Machado",
-      Email: "vanessa.garcia@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Costa Rica",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Mariana Rueda Loyo",
-      Email: "mariana.rueda@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Cordoba",
-      Rol: "Usuario final"
-  },
-  {
-      Nombre: "Isabel Juarez Camarillo",
-      Email: "isabel.juarez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Guadalajara",
-      Rol: "Usuario final"
-  }
-]
+    {
+        Nombre: "Maria Fernanda Mercado Garcia",
+        Email: "fernanda.mercado@optimizacion-online.com",
+        Departamento: "Asistente de direccion general",
+        Oficina: "Leon",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Josselyn Sarahi Nepomuceno",
+        Email: "josselyn.nepomuceno@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Guadalupe Chacon Acosta",
+        Email: "guadalupe.chacon@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Viviana Velasco Montero",
+        Email: "viviana.velasco@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Emmanuel Garcia Solis",
+        Email: "emmanuel.garcia@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Isai Lopez Fuentes",
+        Email: "isai.lopez@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Raul Rios Conde",
+        Email: "raul@optimizacion-online.com",
+        Departamento: "CEO",
+        Oficina: "Leon",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Natalia Daniela Sanchez Garcia",
+        Email: "daniela.garcia@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Colombia",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Flor Noemi Fernandez Ceron",
+        Email: "noemi.fernandez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Ciudad de Mexico",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Irian Valeria Perez Romero",
+        Email: "irian.perez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Ciudad de Mexico",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Victorio Maniel Molina",
+        Email: "victorio.molina@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Argentina",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Emmy Julissa Enriquez Salazar",
+        Email: "emmy.enriquez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Merida",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Vanessa Alexandra Garcia Machado",
+        Email: "vanessa.garcia@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Costa Rica",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Mariana Rueda Loyo",
+        Email: "mariana.rueda@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Cordoba",
+        Rol: "Usuario final"
+    },
+    {
+        Nombre: "Isabel Juarez Camarillo",
+        Email: "isabel.juarez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Guadalajara",
+        Rol: "Usuario final"
+    }
+    ]
 
-interface NoEmpleados {
-  Num?: number;
-  Nombre: string;
-  Email: string;
-  Departamento: string;
-  Oficina: string;
-}
-const NOEMPLEADO: NoEmpleados[] = [
-  {
-      Nombre: "Linda Elena Landis Peña",
-      Email: "elena.landis@optimizacion-online.com",
-      Departamento: "Asistente de direccion general",
-      Oficina: "Leon"
-  },
-  {
-      Nombre: "Jacqueline Villalpando Villegas",
-      Email: "jacqueline@optimizacion-online.com",
-      Departamento: "Asistente de direccion general",
-      Oficina: "Leon"
-  },
-  {
-      Nombre: "Moises C A",
-      Email: "moises.optimizacion@gmail.com",
-      Departamento: "Auditorias SEO",
-      Oficina: "Ciudad de Mexico"
-  },
-  {
-      Nombre: "Alejandra Luna Hilario",
-      Email: "alejandra.luna@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba"
-  },
-  {
-      Nombre: "Jair Sanchez Rivera",
-      Email: "jair.sanchez@optimizacion-online.com",
-      Departamento: "Becarios",
-      Oficina: "Cordoba"
-  },
-  {
-      Nombre: "Demo Demo",
-      Email: "demo@demo.com",
-      Departamento: "CEO",
-      Oficina: "Puebla"
-  },
-  {
-      Nombre: "Carlos Gilberto Meléndez Monárrez",
-      Email: "carlos.meledez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Coatzacoalcos"
-  },
-  {
-      Nombre: "Ximena Herrera Gomez",
-      Email: "ximena.herrera@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Aguascalientes"
-  },
-  {
-      Nombre: "Maria Elena Gomez Gonzalez",
-      Email: "maria.gomez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Puebla"
-  },
-  {
-      Nombre: "Laura Damaris Colunga Grajeda",
-      Email: "laura.colunga@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Leon"
-  },
-  {
-      Nombre: "Thalia Aguirre Marron",
-      Email: "thalia.aguirre@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Leon"
-  },
-  {
-      Nombre: "Sofia Lopez Sanchez",
-      Email: "sofia.loppez@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Guadalajara"
-  },
-  {
-      Nombre: "Mario Cardenas Garcia",
-      Email: "mario.cardenas@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Ciudad de Mexico"
-  },
-  {
-      Nombre: "Jam Jairo Sanguinetii López",
-      Email: "jam.sanguinetii@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Colombia"
-  },
-  {
-      Nombre: "Abraham de Jesus Reyes Arambula",
-      Email: "abraham.reyes@optimizacion-online.com",
-      Departamento: "Consultor de marketing digital",
-      Oficina: "Guadalajara"
-  }
-]
+    interface NoEmpleados {
+    Num?: number;
+    Nombre: string;
+    Email: string;
+    Departamento: string;
+    Oficina: string;
+    }
+    const NOEMPLEADO: NoEmpleados[] = [
+    {
+        Nombre: "Linda Elena Landis Peña",
+        Email: "elena.landis@optimizacion-online.com",
+        Departamento: "Asistente de direccion general",
+        Oficina: "Leon"
+    },
+    {
+        Nombre: "Jacqueline Villalpando Villegas",
+        Email: "jacqueline@optimizacion-online.com",
+        Departamento: "Asistente de direccion general",
+        Oficina: "Leon"
+    },
+    {
+        Nombre: "Moises C A",
+        Email: "moises.optimizacion@gmail.com",
+        Departamento: "Auditorias SEO",
+        Oficina: "Ciudad de Mexico"
+    },
+    {
+        Nombre: "Alejandra Luna Hilario",
+        Email: "alejandra.luna@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba"
+    },
+    {
+        Nombre: "Jair Sanchez Rivera",
+        Email: "jair.sanchez@optimizacion-online.com",
+        Departamento: "Becarios",
+        Oficina: "Cordoba"
+    },
+    {
+        Nombre: "Demo Demo",
+        Email: "demo@demo.com",
+        Departamento: "CEO",
+        Oficina: "Puebla"
+    },
+    {
+        Nombre: "Carlos Gilberto Meléndez Monárrez",
+        Email: "carlos.meledez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Coatzacoalcos"
+    },
+    {
+        Nombre: "Ximena Herrera Gomez",
+        Email: "ximena.herrera@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Aguascalientes"
+    },
+    {
+        Nombre: "Maria Elena Gomez Gonzalez",
+        Email: "maria.gomez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Puebla"
+    },
+    {
+        Nombre: "Laura Damaris Colunga Grajeda",
+        Email: "laura.colunga@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Leon"
+    },
+    {
+        Nombre: "Thalia Aguirre Marron",
+        Email: "thalia.aguirre@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Leon"
+    },
+    {
+        Nombre: "Sofia Lopez Sanchez",
+        Email: "sofia.loppez@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Guadalajara"
+    },
+    {
+        Nombre: "Mario Cardenas Garcia",
+        Email: "mario.cardenas@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Ciudad de Mexico"
+    },
+    {
+        Nombre: "Jam Jairo Sanguinetii López",
+        Email: "jam.sanguinetii@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Colombia"
+    },
+    {
+        Nombre: "Abraham de Jesus Reyes Arambula",
+        Email: "abraham.reyes@optimizacion-online.com",
+        Departamento: "Consultor de marketing digital",
+        Oficina: "Guadalajara"
+    }
+    ]
 
-@Component({
-  selector: 'app-listar-empleado',
-  templateUrl: './listar-empleado.component.html',
-  styleUrls: ['./listar-empleado.component.scss']
-})
+    @Component({
+    selector: 'app-listar-empleado',
+    templateUrl: './listar-empleado.component.html',
+    styleUrls: ['./listar-empleado.component.scss']
+    })
 
-export class ListarEmpleadoComponent{
-  page = 1;
-  pageSize = 10;
-  collectionSize1 = EMPLEADO.length;
-  empleados: Empleados[];
-  collectionSize2 = NOEMPLEADO.length;
-  noempleados: NoEmpleados[];
+    export class ListarEmpleadoComponent{
+    page = 1;
+    pageSize = 10;
+    collectionSize1 = EMPLEADO.length;
+    empleados: Empleados[];
+    collectionSize2 = NOEMPLEADO.length;
+    noempleados: NoEmpleados[];
 
-  constructor() {
-    this.refreshEmpleados();
-    this.refreshNoEmpleados();
-  }
+    constructor() {
+        this.refreshEmpleados();
+        this.refreshNoEmpleados();
+    }
 
-  refreshEmpleados() {
-    this.empleados = EMPLEADO
-      .map((empleados, i) => ({Num: i + 1, ...empleados}))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-  }
+    refreshEmpleados() {
+        this.empleados = EMPLEADO
+        .map((empleados, i) => ({Num: i + 1, ...empleados}))
+        .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    }
 
-  refreshNoEmpleados() {
-    this.noempleados = NOEMPLEADO
-      .map((noempleados, i) => ({Num: i + 1, ...noempleados}))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-  }
+    refreshNoEmpleados() {
+        this.noempleados = NOEMPLEADO
+        .map((noempleados, i) => ({Num: i + 1, ...noempleados}))
+        .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    }
 }
--- src/app/info_clientes/listar-cliente/listar-cliente.component.ts
+++ src/app/info_clientes/listar-cliente/listar-cliente.component.ts
@@ -1,15 +1,236 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes {
+  Num?: number;
+  Dominio: string;
+  UltimoContenido: String;
+  UltimoPago: String;
+  Region: string;
+}
+
+const CLIENTE: Clientes[] = [
+  {
+    Dominio: "www.promesasdevida.com.mx",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2021-11-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "www.wibo.mx",
+    UltimoContenido: "2022-03-14",
+    UltimoPago: "2021-12-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "www.spulindustrial.com.mx",
+    UltimoContenido: "2022-02-10",
+    UltimoPago: "2021-11-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "biera.com.mx/",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2022-01-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "conectix.com/",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2022-02-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "www.rayssa.cl/",
+    UltimoContenido: "2022-01-25",
+    UltimoPago: "2021-12-30",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.cuatrorios.cl",
+    UltimoContenido: "2021-12-25",
+    UltimoPago: "2021-11-30",
+    Region: "Chile"
+  },
+  {
+    Dominio: "royalpro.cl/",
+    UltimoContenido: "2021-12-20",
+    UltimoPago: "2021-02-30",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.thephonehouse.cl",
+    UltimoContenido: "2022-01-20",
+    UltimoPago: "2021-11-30",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.bellapiel.cl",
+    UltimoContenido: "2021-12-20",
+    UltimoPago: "2021-01-30",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.izytech.com/",
+    UltimoContenido: "2021-12-25",
+    UltimoPago: "2021-03-15",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.rwautomatizacion.cl/",
+    UltimoContenido: "2021-12-01",
+    UltimoPago: "2021-03-01",
+    Region: "Chile"
+  },
+  {
+    Dominio: "www.mx.izytech.com/",
+    UltimoContenido: "2021-11-28",
+    UltimoPago: "2022-01-01",
+    Region: "Chile"
+  },
+  {
+    Dominio: "cgcweb.cl/",
+    UltimoContenido: "2021-12-05",
+    UltimoPago: "2022-02-01",
+    Region: "Chile"
+  },
+  {
+    Dominio: "esencialshop.cl/",
+    UltimoContenido: "2022-01-24",
+    UltimoPago: "2022-02-24",
+    Region: "Chile"
+  }
+]
+
+interface NoClientes {
+  Num?: number;
+  Dominio: string;
+  UltimoContenido: String;
+  UltimoPago: String;
+  Region: string;
+}
+
+const NOCLIENTE: NoClientes[] = [
+  {
+    Dominio: "https://www.elcaneloautopartes.com.mx",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2021-11-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.bocapi-muebles.com",
+    UltimoContenido: "2022-03-14",
+    UltimoPago: "2021-12-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.lacreperia.mx/",
+    UltimoContenido: "2022-02-10",
+    UltimoPago: "2021-11-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://cerrito.com.mx/",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2022-01-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.plasma-digital.com.mx/",
+    UltimoContenido: "2022-02-14",
+    UltimoPago: "2022-02-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://dvigna.com.mx/",
+    UltimoContenido: "2022-01-25",
+    UltimoPago: "2021-12-30",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://duelencino.com/",
+    UltimoContenido: "2021-12-25",
+    UltimoPago: "2021-11-30",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://anuncioslumicolor.com/",
+    UltimoContenido: "2021-12-20",
+    UltimoPago: "2021-02-30",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://monteolimpo.mx/",
+    UltimoContenido: "2022-01-20",
+    UltimoPago: "2021-11-30",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.mueblesnovajerez.com/",
+    UltimoContenido: "2021-12-20",
+    UltimoPago: "2021-01-30",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.piochicken.mx/",
+    UltimoContenido: "2021-12-25",
+    UltimoPago: "2021-03-15",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://gruposancristobal.com.mx/",
+    UltimoContenido: "2021-12-01",
+    UltimoPago: "2021-03-01",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://wibo.mx/",
+    UltimoContenido: "2021-11-28",
+    UltimoPago: "2022-01-01",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.gruposancristobal.com.mx",
+    UltimoContenido: "2021-12-05",
+    UltimoPago: "2022-02-01",
+    Region: "Aguascalientes"
+  },
+  {
+    Dominio: "http://www.hotelcasadeljardin.com/",
+    UltimoContenido: "2022-01-24",
+    UltimoPago: "2022-02-24",
+    Region: "Aguascalientes"
+  }
+]
+
 @Component({
   selector: 'app-listar-cliente',
   templateUrl: './listar-cliente.component.html',
   styleUrls: ['./listar-cliente.component.scss']
 })
-export class ListarClienteComponent implements OnInit {
 
-  constructor() { }
+export class ListarClienteComponent{
+  page = 1;
+  pageSize = 10;
+  collectionSize1 = CLIENTE.length;
+  clientes: Clientes[];
+  collectionSize2 = NOCLIENTE.length;
+  noclientes: NoClientes[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.refreshClientes();
+    this.refreshNoClientes();
   }
+
+  refreshClientes() {
+    this.clientes = CLIENTE
+    .map((clientes, i) => ({Num: i + 1, ...clientes}))
+    .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+}
+
+refreshNoClientes() {
+  this.noclientes = NOCLIENTE
+  .map((noclientes, i) => ({Num: i + 1, ...noclientes}))
+  .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+}
 
 }
